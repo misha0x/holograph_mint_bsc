409ce70aa4ad2e71b30e45e012c6823dda4d7d6f
Amina <136509214+amina0x@users.noreply.github.com>
2023-06-15 09:07:51 +0500

    Change to BLDG

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ time_ot = 50
 time_do = 200
 
 # RPCs
-polygon_rpc_url = 'https://polygon-rpc.com/'
+polygon_rpc_url = 'https://rpc.ankr.com/polygon'
 w3 = Web3(Web3.HTTPProvider(polygon_rpc_url))
 # ABIs
 abi = json.load(open('router_abi.json'))
-contr_polygon_address = w3.to_checksum_address('0x4803e859a2e325dc8f6adcd23ea682e323f59640')
+contr_polygon_address = w3.to_checksum_address('0x8C531f965C05Fab8135d951e2aD0ad75CF3405A2')
 nft_contract = w3.eth.contract(address=contr_polygon_address, abi=abi)
 
 with open("privates.txt", "r") as f:
     keys_list = [row.strip() for row in f if row.strip()]
     numbered_keys = list(enumerate(keys_list, start=1))
-    #random.shuffle(numbered_keys)
+    random.shuffle(numbered_keys)
 #counter = 0
 wallets_prob = []
 for wallet_number, PRIVATE_KEY in numbered_keys:
@@ -45,15 +45,15 @@ for wallet_number, PRIVATE_KEY in numbered_keys:
         signed_swap_txn = w3.eth.account.sign_transaction(swap_txn, PRIVATE_KEY)
         swap_txn_hash = w3.eth.send_raw_transaction(signed_swap_txn.rawTransaction)
         print(f"Transaction: https://polygonscan.com/tx/{swap_txn_hash.hex()}")
-        cprint("Вроде бы заебись", "green")
+        cprint("Success", "green")
         time.sleep(random.randint(time_ot, time_do))
     except:
-        cprint("Не заебись", "red")
+        cprint("Failed", "red")
         wallets_prob.append(address)
 
 if len(wallets_prob) > 0:
-    cprint("Есть проблемки", "red")
+    cprint("Problems", "red")
     print(wallets_prob)
 else:
-    cprint("Закончили без ошибок", "green")
+    cprint("All done", "green")
 
